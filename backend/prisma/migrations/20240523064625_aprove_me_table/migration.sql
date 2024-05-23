9ee7af008ffe0d050c325cef93c4a9a1264bcf89
Ruan Chagas <ruanchagas95@gmail.com>
2024-05-23 04:06:42 -0300

    create and migrate sqlite schema

---
+++ backend/prisma/migrations/20240523064625_aprove_me_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Payable" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "emissionDate" DATETIME NOT NULL,
+    "assignor" TEXT NOT NULL,
+    CONSTRAINT "Payable_assignor_fkey" FOREIGN KEY ("assignor") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
